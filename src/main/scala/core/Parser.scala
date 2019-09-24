51185ae5c0803f945c82809a5ded08177d6b67f7
Ketan1990 <ketan.jain@live.com>
2019-09-24 14:26:25 +0530

    added parser for words

--- src/main/scala/core/Parser.scala
+++ src/main/scala/core/Parser.scala
@@ -5,18 +5,21 @@ case class Parser[A](run: String => List[(A, String)]) {
 
   import Parser._
 
-  def map[B](f: A => B): Parser[B] = self.bind(a => result(f(a)))
+  def map[B](f: A => B): Parser[B] = self.flatMap(a => result(f(a)))
 
-  def bind[B](f: A => Parser[B]): Parser[B] =
-    Parser(input =>
-      (for {p <- self.run(input)} yield f(p._1)).flatten
-    )
+  def flatMap[B](f: A => Parser[B]): Parser[B] = bind(self)(f)
+
+  def plus: Parser[A] => Parser[A] =  p2 => {
+    Parser(inp => self.run(inp) ++: p2.run(inp))
+  }
+
+
 }
 
 object Parser {
   def result[A]: A => Parser[A] = a => Parser(input => List((a, input)))
 
-  def zero: Parser[Unit] = Parser(_ => List())
+  def zero[A]: Parser[A] = Parser(_ => List())
 
   def item: Parser[Char] = Parser(inp =>
     inp.toList match {
@@ -24,13 +27,41 @@ object Parser {
       case x :: xs => List((x, xs.mkString))
     })
 
-  def seq[A, B]: Parser[A] => Parser[B] => Parser[(A, B)] = p => q =>
-    Parser(input =>
-      for {
-        a <- p.run(input)
-        b <- q.run(a._2)
-      } yield ((a._1, b._1), a._2)
-    )
+  def seq[A, B]: Parser[A] => Parser[B] => Parser[(A, B)] = p => q => {
+    for {a <- p; b <- q} yield (a, b)
+  }
 
+  def bind[A, B](a: Parser[A])(f: A => Parser[B]): Parser[B] =
+    Parser { input =>
+      (for {(v, inp) <- a.run(input)} yield f(v).run(inp)).flatten
+    }
 
+  def sat(p: Char => Boolean): Parser[Char] = item.flatMap(a => if (p(a)) result(a) else zero)
+
+  /*  Using sat, we can define parsers for specific characters, single digits, lower-case
+    letters, and upper-case letters:*/
+  def char: Char => Parser[Char] = x => {
+    sat(y => x == y)
+  }
+
+  def digit: Parser[Char] = sat(x => '0' <= x && x <= '9')
+
+  def lower: Parser[Char] = sat(x => 'a' <= x && x <= 'z')
+
+  def upper: Parser[Char] = sat(x => 'A' <= x && x <= 'Z')
+
+  def letter:Parser[Char] = lower.plus(upper)
+
+  def alphaNum:Parser[Char] = letter plus digit
+
+  def word: Parser[String] = {
+
+    val newWord: Parser[String] = for {
+      x <- letter
+      y <- word
+    }yield x.toString.concat(y)
+
+    newWord plus result("")
+
+  }
 }
--- src/test/scala/core/ParserSpec.scala
+++ src/test/scala/core/ParserSpec.scala
@@ -10,17 +10,72 @@ class ParserSpec extends FlatSpec with Matchers {
     assertResult(result("a").run(""))(List(("a", "")))
   }
 
-  it should "primitive parser zero always failed " in {
+  it should " failed with zero parser" in {
     assertResult(zero.run(""))(List())
   }
 
-  it should "item primitive parser whcih return first char from input " in {
-    assertResult(item.run("hello"))(List(('h',"ello")))
+  it should "which return first char from input using item primitive parser " in {
+    assertResult(item.run("hello"))(List(('h', "ello")))
     assertResult(item.run(""))(List())
   }
 
-  it should "seq combinator sequencing parser " in {
-    assertResult(seq(item)(item).run("hello"))(List((('h','e'),"ello")))
+  it should "seq  parser " in {
+    assertResult(seq(item)(item).run("hello"))(List((('h', 'e'), "llo")))
+  }
+  it should "bind  monadic sequencing combinator for sequencing parsers" in {
+    val actual = bind(item)(a => result(a)).run("hello")
+    assertResult(actual)(List(('h', "ello")))
+  }
+  it should "sat that takes predicates it return result if satisfies and fails otherwise " in {
+    val actual1 = sat(a => a == 'h').run("hello")
+    assertResult(actual1)(List(('h', "ello")))
+
+    val actual2 = sat(a => a == 'h').run("ello")
+    assertResult(actual2)(List())
+  }
+
+  it can "define parsers for specific characters" in {
+    val actual = char('h').run("hllo")
+    assertResult(actual)(List(('h', "llo")))
+  }
+
+  it can "define parsers for specific digit" in {
+    val actual = char('9').run("904323")
+    assertResult(actual)(List(('9', "04323")))
+  }
+
+  it can "define parsers for specific lower" in {
+    val actual = lower.run("helo")
+    assertResult(actual)(List(('h', "elo")))
+  }
+
+  it can "define parsers for specific upper" in {
+    val actual = upper.run("Helo")
+    assertResult(actual)(List(('H', "elo")))
+    assertResult(upper.run("helo"))(List())
+  }
+
+  it should "choice between parsers with plus combinator" in {
+    val lowerUpper = lower.plus(upper)
+    assertResult(lowerUpper.run("Bcd"))(List(('B', "cd")))
+    assertResult(lowerUpper.run("cd"))(List(('c', "d")))
+  }
+
+  it should "letter combinator" in {
+    assertResult(letter.run("Bcd"))(List(('B', "cd")))
+    assertResult(letter.run("cd"))(List(('c', "d")))
+  }
+  it should "alphaNum combinator" in {
+    assertResult(alphaNum.run("Bcd"))(List(('B', "cd")))
+    assertResult(alphaNum.run("1d"))(List(('1', "d")))
+  }
+
+  it should "word parser" in {
+
+    assertResult(word.run("Yes!"))(List(("Yes", "!"),
+      ("Ye", "s!"), ("Y", "es!"), ("", "Yes!")))
+
+    assertResult(word.run("121"))(List(("", "121")))
   }
 
 }
